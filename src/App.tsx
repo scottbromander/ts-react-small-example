be1e6ee54d50196525b03819989a6818861f1c6e
scottbromander <scottbromander@gmail.com>
2020-10-10 22:17:52 -0500

    complex types

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
 
 class App extends Component {
   render() {
-    // let firstValue: string = 'Scott';
-    // let firstValue: number = 34;
-    // let firstValue: number[] = [3, 4];
-    let firstValue: Array<string> = ['S', 'c', 'o', 't', 't'];
+    // BASIC TYPES
+    // let firstValue: string = 'Scott'; // String
+    // let firstValue: number = 34; // Number
+    // let firstValue: boolean = true; // Boolean
+    // let firstValue: number[] = [3, 4]; // Array creation 1
+    // let firstValue: Array<string> = ['S', 'c', 'o', 't', 't']; // Array creation 2
+
+    // Complex Types
+    // Tuple
+    // let aTuple: [string, number] = ['Scott', 37];
+    // Enums
+    // enum Codes { first = 1, second = 2 };
+    // Any - Sort of defeats the purpose of TS
+    // let firstName: any = 'Scott'
+    // Voids
+    // const warning = (): void => console.log('warn');
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          {/* <p>
             The value {firstValue} is of {typeof firstValue} type!
-          </p>
+          </p> */}
         </header>
       </div>
     );
